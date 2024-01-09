c25473e89f687c8c3ceaec03e33a2d3818b5ba63
Ojo Oluwatobi <ojot630@gmail.com>
2024-01-09 18:30:42 +0100

    added run api

--- src/main/java/org/semicolon/controller/TodoListController.java
+++ src/main/java/org/semicolon/controller/TodoListController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/user")
+//@RequestMapping("/api/user")
 public class TodoListController {
     @Autowired
     private TodoListService todoListService;
